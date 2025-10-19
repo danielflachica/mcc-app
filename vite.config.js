42d67e573161df7d033a5db01965ae175a1cf3e4
Daniel Lachica <daniel.lachica82@gmail.com>
2025-10-19 18:54:25 +0800

    Install bootstrap via npm

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true, // hide bootstrap deprecation warnings
+            },
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });

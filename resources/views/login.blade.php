53c1a8544af360518cfabe319f4fb0300b1df557
Daniel Lachica <daniel.lachica82@gmail.com>
2025-10-21 17:44:47 +0800

    Add glassmorphism to public pages

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.public')
 
 @section('title', 'Login | ' . config('app.name', 'Mindcare Club'))
 
+@section('hero')
+<div class="row m-0 d-flex align-items-center justify-content-center pt-4" id="bg-landing-img">
+    <div class="mt-5 py-4 px-5 w-auto glass">
+        <h1 class="text-white">Start your journey</h1>
+    </div>
+</div>
+@endsection
+
 @section('content')
 
-<x-card>
-    <x-slot name="header">
-        <h3>Login</h3>
-    </x-slot>
+<div class="row pt-3">
+    <div class="col col-12 col-md-8 col-lg-6 mx-auto">
+        <x-card>
+            <x-slot name="header">
+                <h3>Login</h3>
+            </x-slot>
 
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="loginEmail" class="form-label">Email</label>
-            <input type="email" class="form-control" id="loginEmail" name="loginEmail">
-        </div>
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="loginEmail" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="loginEmail" name="loginEmail">
+                </div>
 
-        <div class="mb-3">
-            <label for="loginPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="loginPassword" name="loginPassword">
-        </div>
+                <div class="mb-3">
+                    <label for="loginPassword" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="loginPassword" name="loginPassword">
+                </div>
 
-        <button type="submit" class="btn btn-secondary">Login</button>
-    </form>
-</x-card>
+                <button type="submit" class="btn btn-secondary">Login</button>
+            </form>
+        </x-card>
+    </div>
+</div>
 
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,31 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.public')
 
 @section('title', 'Register | ' . config('app.name', 'Mindcare Club'))
 
+@section('hero')
+<div class="row m-0 d-flex align-items-center justify-content-center pt-4" id="bg-landing-img">
+    <div class="mt-5 py-4 px-5 w-auto glass">
+        <h1 class="text-white">Create an Account</h1>
+    </div>
+</div>
+@endsection
+
 @section('content')
 
-<x-card>
-    <x-slot name="header">
-        <h3>Register</h3>
-    </x-slot>
+<div class="row pt-3">
+    <div class="col col-12 col-md-8 col-lg-6 mx-auto">
+        <x-card>
+            <x-slot name="header">
+                <h3>Register</h3>
+            </x-slot>
 
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="inputName" class="form-label">Name</label>
-            <input type="text" class="form-control" id="inputName" name="name">
-        </div>
-        <div class="mb-3">
-            <label for="inputEmail" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail" name="email" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="inputPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword" name="password">
-        </div>
-        <button type="submit" class="btn btn-secondary">Register</button>
-    </form>
-</x-card>
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="inputName" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="inputName" name="name">
+                </div>
+                <div class="mb-3">
+                    <label for="inputEmail" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail" name="email" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" name="password">
+                </div>
+                <button type="submit" class="btn btn-secondary">Create Account</button>
+            </form>
+        </x-card>
+    </div>
+</div>
 
 @endsection
